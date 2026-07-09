05ba5330bb70e0d2980249a458eb96c24b9819c4
BlackCrystalic <39885003+BlackCrystalic@users.noreply.github.com>
2026-07-09 13:32:57 +0500

    final?

--- src/handler.cpp
+++ src/handler.cpp
@@ -19,11 +19,14 @@ namespace StatisticSystemComponent {
 	using namespace RC::Unreal;
 
 	static UClass* DinoClass{};
-
 	static ATIGameModeBase* GameMode{};
 
 	static int TicksFired{0};
 	static constexpr int PerTicksFired{100};// how often we save data (every min)
+
+// If server wont restart very often, like 20 hours and players would die like every 30 seconds
+// then go for adding unordered_map and use it to store time, delay for clean supposed to be around 20 minutes
+	std::unordered_set<int> DeathList{};
 
 	auto Fire() -> void {
 		if (!GameMode) {// It fires before we have gamemode, need to solve that
@@ -37,31 +40,32 @@ namespace StatisticSystemComponent {
 		for (ATICharacterBase* Character : ActivePlayers) {
 			if (!Character || !Character->IsA(DinoClass)) continue;// Make sure it's actually dino, not a fucking damn human
 
-			ATIDinosaurBase* Dino = static_cast<ATIDinosaurBase*>(Character);
-			int32 DinoID = Dino->GetID();
-			if (Dino->GetbIsDead()) {
+			ATIDinosaurBase* Dinosaur = static_cast<ATIDinosaurBase*>(Character);
+			int32 DinoID = Dinosaur->GetID();
+			if (Dinosaur->GetbIsDead() && !DeathList.contains(DinoID)) {
+				DeathList.insert(DinoID);
 				DataBaseConnector::DinoDied(DinoID);
 				continue;
 			}
 
-			if (!TicksFired && Dino->GetSteamId().IsEmpty()) {
+			if (!TicksFired && Dinosaur->GetSteamId().IsEmpty()) {
 				FTIPlayerData PlayerData = UTISaveManager::GetCharacterData(Character, false);
 				FString Result = UTISaveManager::PlayerDataToString(PlayerData);
-				FString SteamID = Dino->GetSteamId();
+				FString SteamID = Dinosaur->GetSteamId();
 				DataBaseConnector::SaveDino(SteamID, DinoID, Result, true);
 			}
 
 			Batch.DinoID.push_back(std::to_string(static_cast<int>(DinoID)));
-			Batch.Growth.push_back(std::to_string(Dino->GetGrowth()));
+			Batch.Growth.push_back(std::to_string(Dinosaur->GetGrowth()));
 
-			Batch.Health.push_back(std::to_string(Dino->GetHealth()));
-			Batch.Stamina.push_back(std::to_string(Dino->GetStamina()));
-			Batch.Hunger.push_back(std::to_string(Dino->GetHunger()));
-			Batch.Thirst.push_back(std::to_string(Dino->GetThirst()));
-			Batch.Oxygen.push_back(std::to_string(Dino->GetOxygen()));
-			Batch.Blood.push_back(std::to_string(Dino->GetBlood()));
+			Batch.Health.push_back(std::to_string(Dinosaur->FGetHealth()));
+			Batch.Stamina.push_back(std::to_string(Dinosaur->FGetStamina()));
+			Batch.Hunger.push_back(std::to_string(Dinosaur->FGetHunger()));
+			Batch.Thirst.push_back(std::to_string(Dinosaur->FGetThirst()));
+			Batch.Oxygen.push_back(std::to_string(Dinosaur->FGetOxygen()));
+			Batch.Blood.push_back(std::to_string(Dinosaur->FGetBlood()));
 
-			FVector PlayerVector = Dino->K2_GetActorLocation();
+			FVector PlayerVector = Dinosaur->K2_GetActorLocation();
 			Batch.X.push_back(std::to_string(static_cast<float>(PlayerVector.X())));
 			Batch.Y.push_back(std::to_string(static_cast<float>(PlayerVector.Y())));
 			Batch.Z.push_back(std::to_string(static_cast<float>(PlayerVector.Z())));
