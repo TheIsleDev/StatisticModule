3bc4d6a929bc4f53da838b2c7a6770d732dab316
BlackCrystalic <39885003+BlackCrystalic@users.noreply.github.com>
2026-07-27 11:20:13 +0500

    Fix of wrong local version

--- src/handler.cpp
+++ src/handler.cpp
@@ -29,8 +29,14 @@ namespace StatisticSystemComponent {
 	DelegateManager BindingManager{};
 	UFunction* FHandleCharacterDeath{};
 
-	UFunction* PlayerRespawned{};
-	std::pair<int, int> PlayerSpawnCallBackIDs{};
+	UFunction* FOnPlayerRespawned{};
+	std::pair<int, int> FOnPlayerRespawnedIDs{};
+
+	UFunction* FAttemptDestroy{};
+	std::pair<int, int> FAttemptDestroyIDs{};
+
+	UFunction* FAutosave{};
+	std::pair<int, int> FAutosaveIDs{};
 
 	int TicksFired{0};
 	static constexpr int PerTicksFired{60};// how often we save data (every min)
@@ -42,12 +48,6 @@ namespace StatisticSystemComponent {
 
 		StatisticStructs::StatisticBatch Batch;
 		for (ATIDinosaurBase* Dinosaur : Dinosaurs) {
-			RC::Output::send<RC::LogLevel::Verbose>(STR("Fired"));
-			// Make it save on their own delay, not global #FUTURE #NOLAZINES
-			if (!TicksFired && !Dinosaur->GetSteamId().IsEmpty()) {
-				DataBaseConnector::SaveDino(Dinosaur, true, false);
-			}
-
 			Batch.SteamID.push_back(RC::to_string(*Dinosaur->GetSteamId()));
 			Batch.Class.push_back(RC::to_string(Dinosaur->GetClassPrivate()->GetPathName()));
 			Batch.DinoID.push_back(std::to_string(Dinosaur->GetID()));
@@ -71,9 +71,9 @@ namespace StatisticSystemComponent {
 		DataBaseConnector::StoreStatisticBatch(Batch);
 	}
 
+
 	auto HandleCharacterDeath(UObject* Context, FFrame& Stack, void* Result) -> void {
 		RC::Output::send<RC::LogLevel::Verbose>(STR("Noob died"));
-
 		ATIDinosaurBase* Dinosaur = *reinterpret_cast<ATIDinosaurBase**>(Stack.Locals());
 		Dinosaurs.Remove(Dinosaur);
 		BindingManager.UnBind(Dinosaur->GetOnCharacterDied());
@@ -83,8 +83,7 @@ namespace StatisticSystemComponent {
 	auto PreCharacterSpawn(UnrealScriptFunctionCallableContext& context, void*) -> void {}
 
 	auto PostCharacterSpawn(UnrealScriptFunctionCallableContext& context, void*) -> void {
-		RC::Output::send<RC::LogLevel::Verbose>(STR("Noob added"));
-
+		RC::Output::send<RC::LogLevel::Verbose>(STR("Noob spawned"));
 		APlayerController* Controller = *reinterpret_cast<APlayerController**>(context.TheStack.Locals());
 		APawn* Pawn = Controller->GetPawn();
 		if (!Pawn || !Pawn->IsA(ATIDinosaurBase::StaticClass())) return;
@@ -92,8 +91,30 @@ namespace StatisticSystemComponent {
 		ATIDinosaurBase* Dinosaur = static_cast<ATIDinosaurBase*>(Pawn);
 		Dinosaurs.Add(Dinosaur);
 		BindingManager.Bind(Dinosaur->GetOnCharacterDied(), CallBackSucker, FHandleCharacterDeath->GetFName());
+	}
+
+
+	auto PreCharacterDestroy(UnrealScriptFunctionCallableContext& context, void*) -> void {
+		RC::Output::send<RC::LogLevel::Verbose>(STR("Noob destroyed"));
+		if (!context.Context->IsA(ATIDinosaurBase::StaticClass())) return;
+
+		ATIDinosaurBase* Dinosaur = static_cast<ATIDinosaurBase*>(context.Context);
+		Dinosaurs.Remove(Dinosaur);
+	}
+
+	auto PostCharacterDestroy(UnrealScriptFunctionCallableContext& context, void*) -> void {}
+
+
+	auto PreAutosave(UnrealScriptFunctionCallableContext& context, void*) -> void {
+		RC::Output::send<RC::LogLevel::Verbose>(STR("Noob saved"));
+		if (!context.Context->IsA(ATIDinosaurBase::StaticClass())) return;
+
+		ATIDinosaurBase* Dinosaur = static_cast<ATIDinosaurBase*>(context.Context);
 		DataBaseConnector::SaveDino(Dinosaur, true, false);
 	}
+
+	auto PostAutosave(UnrealScriptFunctionCallableContext& context, void*) -> void {}
+
 
 	auto Initialize(StatisticSystemConfig::StatisticConfig Config) -> void {
 		if (DataBaseConnector::Initialize(Config.Database)) DataBaseConnector::PrepareStatistic();
@@ -101,9 +122,14 @@ namespace StatisticSystemComponent {
 		else RC::Output::send<RC::LogLevel::Error>(STR("DB connection failed, con string: {}"), RC::to_wstring(Config.Database));
 #endif
 
+		FOnPlayerRespawned = UObjectGlobals::StaticFindObject<UFunction*>(nullptr, nullptr, STR("/Game/TheIsle/Core/GameModes/BP_SurvivalGameMode.BP_SurvivalGameMode_C:OnPlayerRespawned"));
+		FOnPlayerRespawnedIDs = UObjectGlobals::RegisterHook(FOnPlayerRespawned, &PreCharacterSpawn, &PostCharacterSpawn, nullptr);
 
-		PlayerRespawned = UObjectGlobals::StaticFindObject<UFunction*>(nullptr, nullptr, STR("/Game/TheIsle/Core/GameModes/BP_SurvivalGameMode.BP_SurvivalGameMode_C:OnPlayerRespawned"));
-		PlayerSpawnCallBackIDs = UObjectGlobals::RegisterHook(PlayerRespawned, &PreCharacterSpawn, &PostCharacterSpawn, nullptr);
+		FAttemptDestroy = UObjectGlobals::StaticFindObject<UFunction*>(nullptr, nullptr, STR("/Script/TheIsle.TICharacterBase:AttemptDestroy"));
+		FAttemptDestroyIDs = UObjectGlobals::RegisterHook(FAttemptDestroy, &PreCharacterDestroy, &PostCharacterDestroy, nullptr);
+
+		FAutosave = UObjectGlobals::StaticFindObject<UFunction*>(nullptr, nullptr, STR("/Script/TheIsle.TICharacterBase:Autosave"));
+		FAutosaveIDs = UObjectGlobals::RegisterHook(FAutosave, &PreAutosave, &PostAutosave, nullptr);
 
 		UClass* ObjectClass = UObjectGlobals::StaticFindObject<UClass*>(nullptr, nullptr, STR("/Script/CoreUObject.Object"));
 		FStaticConstructObjectParameters OParams{ObjectClass};
@@ -116,7 +142,7 @@ namespace StatisticSystemComponent {
 		FParams.Name = FName(STR("HandleCharacterDeath"), FNAME_Add);
 		FParams.Template = Signature;
 
-		FHandleCharacterDeath = UObjectGlobals::StaticFindObject<UFunction*>(nullptr, nullptr, STR("/Script/TheIsle.CharacterDiedDelegate__DelegateSignature"));
+		FHandleCharacterDeath = UObjectGlobals::StaticFindObject<UFunction*>(nullptr, nullptr, STR("/Script/CoreUObject.Object:HandleCharacterDeath"));
 		if (FHandleCharacterDeath) {
 			FHandleCharacterDeath->SetFuncPtr(&HandleCharacterDeath);
 			return;
@@ -137,6 +163,8 @@ namespace StatisticSystemComponent {
 	auto Destroy() -> void {
 		DataBaseConnector::Destroy();
 		BindingManager.Destroy();
-		UObjectGlobals::UnregisterHook(PlayerRespawned, PlayerSpawnCallBackIDs);
+		UObjectGlobals::UnregisterHook(FOnPlayerRespawned, FOnPlayerRespawnedIDs);
+		UObjectGlobals::UnregisterHook(FAttemptDestroy, FAttemptDestroyIDs);
+		UObjectGlobals::UnregisterHook(FAutosave, FAutosaveIDs);
 	}
 }
