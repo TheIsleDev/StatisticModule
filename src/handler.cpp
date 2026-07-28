677f52afb464dc48f0c57768e780fa60bffb06bf
BlackCrystalic <39885003+BlackCrystalic@users.noreply.github.com>
2026-07-28 19:20:52 +0500

    New content for callbacks register

--- src/handler.cpp
+++ src/handler.cpp
@@ -3,6 +3,7 @@
 
 #include <Unreal/AActor.hpp>
 #include <Unreal/UObject.hpp>
+#include <Unreal/UPackage.hpp>
 #include <Unreal/UObjectArray.hpp>
 #include <Unreal/UObjectGlobals.hpp>
 #include <Unreal/CoreUObject/UObject/UnrealType.hpp>
@@ -27,24 +28,20 @@ namespace StatisticSystemComponent {
 	// Fucking pulley for da shit callback?
 	UObject* CallBackSucker{};
 	DelegateManager BindingManager{};
-	UFunction* FHandleCharacterDeath{};
+	UFunction* FHandleCharacterDied{};
+	UFunction* FHandleActorDestroyed{};
 
 	UFunction* FOnPlayerRespawned{};
 	std::pair<int, int> FOnPlayerRespawnedIDs{};
 
-	UFunction* FAttemptDestroy{};
-	std::pair<int, int> FAttemptDestroyIDs{};
 
-	UFunction* FAutosave{};
-	std::pair<int, int> FAutosaveIDs{};
-
-	int TicksFired{0};
-	static constexpr int PerTicksFired{60};// how often we save data (every min)
 	auto Fire() -> void {
 		if (!GameMode) {// It fires before we have gamemode, need to solve that
 			GameMode = static_cast<ATIGameModeBase*>(UObjectGlobals::FindFirstOf(STR("BP_SurvivalGameMode_C")));
 			if(!GameMode) return;
 		}
+
+		RC::Output::send<RC::LogLevel::Verbose>(STR("Noobs num: {}"), Dinosaurs.Num());
 
 		StatisticStructs::StatisticBatch Batch;
 		for (ATIDinosaurBase* Dinosaur : Dinosaurs) {
@@ -66,22 +63,34 @@ namespace StatisticSystemComponent {
 			Batch.Z.push_back(std::to_string(PlayerVector.Z()));
 		}
 
-		if (++TicksFired > PerTicksFired) TicksFired = 0;
 		if (!Batch.Size()) return;
 		DataBaseConnector::StoreStatisticBatch(Batch);
 	}
 
 
-	auto HandleCharacterDeath(UObject* Context, FFrame& Stack, void* Result) -> void {
+	// Наша кастомная функция, для игры это поинтер на память в библиотеку
+	auto HandleActorDestroyed(UObject* Context, FFrame& Stack, void* Result) -> void {
+		RC::Output::send<RC::LogLevel::Verbose>(STR("Noob destroyed"));
+		ATIDinosaurBase* Dinosaur = *reinterpret_cast<ATIDinosaurBase**>(Stack.Locals());
+		Dinosaurs.Remove(Dinosaur);
+		BindingManager.UnBind(Dinosaur->GetOnCharacterDied());
+		BindingManager.UnBind(Dinosaur->GetOnDestroyed());
+	}
+
+
+	// Наша кастомная функция, для игры это поинтер на память в библиотеку
+	auto HandleCharacterDied(UObject* Context, FFrame& Stack, void* Result) -> void {
 		RC::Output::send<RC::LogLevel::Verbose>(STR("Noob died"));
 		ATIDinosaurBase* Dinosaur = *reinterpret_cast<ATIDinosaurBase**>(Stack.Locals());
 		Dinosaurs.Remove(Dinosaur);
 		BindingManager.UnBind(Dinosaur->GetOnCharacterDied());
+		BindingManager.UnBind(Dinosaur->GetOnDestroyed());
 		DataBaseConnector::SaveDino(Dinosaur, false, true);
 	}
 
 	auto PreCharacterSpawn(UnrealScriptFunctionCallableContext& context, void*) -> void {}
 
+	// Вызывается на контроллере когда он получает actor под контроль, почему так? хз.
 	auto PostCharacterSpawn(UnrealScriptFunctionCallableContext& context, void*) -> void {
 		RC::Output::send<RC::LogLevel::Verbose>(STR("Noob spawned"));
 		APlayerController* Controller = *reinterpret_cast<APlayerController**>(context.TheStack.Locals());
@@ -89,32 +98,108 @@ namespace StatisticSystemComponent {
 		if (!Pawn || !Pawn->IsA(ATIDinosaurBase::StaticClass())) return;
 
 		ATIDinosaurBase* Dinosaur = static_cast<ATIDinosaurBase*>(Pawn);
+		if (Dinosaurs.Contains(Dinosaur)) return;// Режим спектатора тоже тригерит это, как и перезаход после hard выхода с сервера
+
 		Dinosaurs.Add(Dinosaur);
-		BindingManager.Bind(Dinosaur->GetOnCharacterDied(), CallBackSucker, FHandleCharacterDeath->GetFName());
+		BindingManager.Bind(Dinosaur->GetOnCharacterDied(), CallBackSucker, FHandleCharacterDied->GetFName());
+		BindingManager.Bind(Dinosaur->GetOnDestroyed(), CallBackSucker, FHandleActorDestroyed->GetFName());
 	}
 
 
-	auto PreCharacterDestroy(UnrealScriptFunctionCallableContext& context, void*) -> void {
-		RC::Output::send<RC::LogLevel::Verbose>(STR("Noob destroyed"));
-		if (!context.Context->IsA(ATIDinosaurBase::StaticClass())) return;
+	auto GenerateClass(UPackage* Package) -> UClass* {
+		// Санити чек)))
+		UClass* UCustom = UObjectGlobals::StaticFindObject<UClass*>(nullptr, nullptr, STR("/Script/TheIsle.ModClassProxy"));
+		if (UCustom) {
+			return UCustom;
+		}
 
-		ATIDinosaurBase* Dinosaur = static_cast<ATIDinosaurBase*>(context.Context);
-		Dinosaurs.Remove(Dinosaur);
+		// Начало создания кастомного класса для того что бы у нас функция была приватная и легче было выгружать мод без перезапуска сервера
+		// TODO: Перенести это все в кастомный модуль что упростит работу, хотя хз что будет быстрее, кастомный мод лоадер или это
+		// В кастомном модлоадере я смогу обращаться проще к другой функции да и там схема будет немного другой, по этому не думаю что стоит тратить на это время щас
+		FStaticConstructObjectParameters UClassParams{UClass::StaticClass(), Package};
+		UClassParams.Name = FName(STR("ModClassProxy"), FNAME_Add);
+		UClassParams.SetFlags = UObject::StaticClass()->GetObjectFlags();
+		UClassParams.Template = UObject::StaticClass();// Копируем основу
+
+		// Регестрирует наш чертов класс
+		UCustom = static_cast<UClass*>(UObjectGlobals::StaticConstructObject(UClassParams));
+
+		// Ересь? Да!
+		UCustom->GetClassConstructor() = UObject::StaticClass()->GetClassConstructor();
+		UCustom->GetClassWithin() = UObject::StaticClass()->GetClassWithin();
+		UCustom->GetClassCastFlags() = UObject::StaticClass()->GetClassCastFlags();
+		UCustom->GetChildProperties() = UObject::StaticClass()->GetChildProperties();
+		UCustom->GetPropertiesSize() = UObject::StaticClass()->GetPropertiesSize();
+		return UCustom;
 	}
 
-	auto PostCharacterDestroy(UnrealScriptFunctionCallableContext& context, void*) -> void {}
+	auto GenerateFunction1(UClass* UCustom) -> void {
+		// Если вдруг мы уже имеем регнутую, иначе может крашнуть...
+		FHandleCharacterDied = UObjectGlobals::StaticFindObject<UFunction*>(nullptr, nullptr, STR("/Script/TheIsle.ModClassProxy:HandleCharacterDeath"));
+		if (FHandleCharacterDied) {
+			FHandleCharacterDied->SetFuncPtr(&HandleCharacterDied);
+			return;
+		}
 
+		// Пример для копирования значений что бы не регать разную хуету, заебусь блять регать все значения сам каждый раз
+		UFunction* FCharacterDiedDelegate = UObjectGlobals::StaticFindObject<UFunction*>(nullptr, nullptr, STR("/Script/TheIsle.CharacterDiedDelegate__DelegateSignature"));
 
-	auto PreAutosave(UnrealScriptFunctionCallableContext& context, void*) -> void {
-		RC::Output::send<RC::LogLevel::Verbose>(STR("Noob saved"));
-		if (!context.Context->IsA(ATIDinosaurBase::StaticClass())) return;
+		// То бля, во бля... короче функция так же как и кастомный класс
+		FStaticConstructObjectParameters FParams{UFunction::StaticClass(), UCustom};
+		FParams.Name = FName(STR("HandleCharacterDied"), FNAME_Add);
+		FParams.SetFlags = UFunction::StaticClass()->GetObjectFlags();
+		FParams.Template = FCharacterDiedDelegate;// Копирует всякие приколы с базы
 
-		ATIDinosaurBase* Dinosaur = static_cast<ATIDinosaurBase*>(context.Context);
-		DataBaseConnector::SaveDino(Dinosaur, true, false);
+		// Регестрирует нашу функцию
+		FHandleCharacterDied = static_cast<UFunction*>(UObjectGlobals::StaticConstructObject(FParams));
+
+		// Копируем базовые праметры, это у нас деменшены функции в памяти и дименшены параметров что передаются в неё
+		FHandleCharacterDied->GetNumParms() = FCharacterDiedDelegate->GetNumParms();
+		FHandleCharacterDied->GetParmsSize() = FCharacterDiedDelegate->GetParmsSize();
+		FHandleCharacterDied->GetChildProperties() = FCharacterDiedDelegate->GetChildProperties();
+		FHandleCharacterDied->GetPropertiesSize() = FCharacterDiedDelegate->GetPropertiesSize();
+
+		// Добавляем наши флаги
+		FHandleCharacterDied->GetFunctionFlags() = (FCharacterDiedDelegate->GetFunctionFlags() | FUNC_Native | FUNC_Public) & ~(FUNC_Delegate | FUNC_MulticastDelegate);
+
+		// Делаем наш ебаный указатель, это вызов wrapper для доступа к памяти самой функции
+		FHandleCharacterDied->SetFuncPtr(&HandleCharacterDied);
+		UCustom->GetFuncMap().Add(FHandleCharacterDied->GetFName(), TObjectPtr<UFunction>(FHandleCharacterDied));
 	}
 
-	auto PostAutosave(UnrealScriptFunctionCallableContext& context, void*) -> void {}
+	auto GenerateFunction2(UClass* UCustom) -> void {
+		// Если вдруг мы уже имеем регнутую, иначе может крашнуть...
+		FHandleActorDestroyed = UObjectGlobals::StaticFindObject<UFunction*>(nullptr, nullptr, STR("/Script/TheIsle.ModClassProxy:HandleActorDestroyed"));
+		if (FHandleActorDestroyed) {
+			FHandleActorDestroyed->SetFuncPtr(&HandleActorDestroyed);
+			return;
+		}
 
+		// Пример для копирования значений что бы не регать разную хуету, заебусь блять регать все значения сам каждый раз
+		UFunction* ActorDestroyedSignature = UObjectGlobals::StaticFindObject<UFunction*>(nullptr, nullptr, STR("/Script/Engine.ActorDestroyedSignature__DelegateSignature"));
+
+		// То бля, во бля... короче функция так же как и кастомный класс
+		FStaticConstructObjectParameters FParams{UFunction::StaticClass(), UCustom};
+		FParams.Name = FName(STR("HandleActorDestroyed"), FNAME_Add);
+		FParams.SetFlags = UFunction::StaticClass()->GetObjectFlags();
+		FParams.Template = ActorDestroyedSignature;// Копирует всякие приколы с базы
+
+		// Регестрирует нашу функцию
+		FHandleActorDestroyed = static_cast<UFunction*>(UObjectGlobals::StaticConstructObject(FParams));
+
+		// Копируем базовые праметры, это у нас деменшены функции в памяти и дименшены параметров что передаются в неё
+		FHandleActorDestroyed->GetNumParms() = ActorDestroyedSignature->GetNumParms();
+		FHandleActorDestroyed->GetParmsSize() = ActorDestroyedSignature->GetParmsSize();
+		FHandleActorDestroyed->GetChildProperties() = ActorDestroyedSignature->GetChildProperties();
+		FHandleActorDestroyed->GetPropertiesSize() = ActorDestroyedSignature->GetPropertiesSize();
+
+		// Добавляем наши флаги
+		FHandleActorDestroyed->GetFunctionFlags() = (ActorDestroyedSignature->GetFunctionFlags() | FUNC_Native | FUNC_Public) & ~(FUNC_Delegate | FUNC_MulticastDelegate);
+
+		// Делаем наш ебаный указатель, это вызов wrapper для доступа к памяти самой функции
+		FHandleActorDestroyed->SetFuncPtr(&HandleActorDestroyed);
+		UCustom->GetFuncMap().Add(FHandleActorDestroyed->GetFName(), TObjectPtr<UFunction>(FHandleActorDestroyed));
+	}
 
 	auto Initialize(StatisticSystemConfig::StatisticConfig Config) -> void {
 		if (DataBaseConnector::Initialize(Config.Database)) DataBaseConnector::PrepareStatistic();
@@ -125,46 +210,24 @@ namespace StatisticSystemComponent {
 		FOnPlayerRespawned = UObjectGlobals::StaticFindObject<UFunction*>(nullptr, nullptr, STR("/Game/TheIsle/Core/GameModes/BP_SurvivalGameMode.BP_SurvivalGameMode_C:OnPlayerRespawned"));
 		FOnPlayerRespawnedIDs = UObjectGlobals::RegisterHook(FOnPlayerRespawned, &PreCharacterSpawn, &PostCharacterSpawn, nullptr);
 
-		FAttemptDestroy = UObjectGlobals::StaticFindObject<UFunction*>(nullptr, nullptr, STR("/Script/TheIsle.TICharacterBase:AttemptDestroy"));
-		FAttemptDestroyIDs = UObjectGlobals::RegisterHook(FAttemptDestroy, &PreCharacterDestroy, &PostCharacterDestroy, nullptr);
+		// Я ебал рот, его крашит если пытаться самому лукапить пакет ебаный, ну будут через прокси искать, хули мне
+		UClass* PackageSource = UObjectGlobals::StaticFindObject<UClass*>(nullptr, nullptr, STR("/Script/TheIsle.ActorPoolManager"));
+		UPackage* FoundPackage = static_cast<UPackage*>(PackageSource->GetOutermost());
 
-		FAutosave = UObjectGlobals::StaticFindObject<UFunction*>(nullptr, nullptr, STR("/Script/TheIsle.TICharacterBase:Autosave"));
-		FAutosaveIDs = UObjectGlobals::RegisterHook(FAutosave, &PreAutosave, &PostAutosave, nullptr);
+		UClass* UCustom = GenerateClass(FoundPackage);
+		GenerateFunction1(UCustom);
+		GenerateFunction2(UCustom);
 
-		UClass* ObjectClass = UObjectGlobals::StaticFindObject<UClass*>(nullptr, nullptr, STR("/Script/CoreUObject.Object"));
-		FStaticConstructObjectParameters OParams{ObjectClass};
-		OParams.Name  = FName(STR("ModDelegateProxy"), FNAME_Add);
-		CallBackSucker = UObjectGlobals::StaticConstructObject(OParams);
-
-		UFunction* Signature = UObjectGlobals::StaticFindObject<UFunction*>(nullptr, nullptr, STR("/Script/TheIsle.CharacterDiedDelegate__DelegateSignature"));
-		UClass* FunctionClass = UObjectGlobals::StaticFindObject<UClass*>(nullptr, nullptr, STR("/Script/CoreUObject.Function"));
-		FStaticConstructObjectParameters FParams{FunctionClass, ObjectClass};
-		FParams.Name = FName(STR("HandleCharacterDeath"), FNAME_Add);
-		FParams.Template = Signature;
-
-		FHandleCharacterDeath = UObjectGlobals::StaticFindObject<UFunction*>(nullptr, nullptr, STR("/Script/CoreUObject.Object:HandleCharacterDeath"));
-		if (FHandleCharacterDeath) {
-			FHandleCharacterDeath->SetFuncPtr(&HandleCharacterDeath);
-			return;
-		}
-
-		FHandleCharacterDeath = static_cast<UFunction*>(UObjectGlobals::StaticConstructObject(FParams));
-
-		FHandleCharacterDeath->GetNumParms() = Signature->GetNumParms();
-		FHandleCharacterDeath->GetParmsSize() = Signature->GetParmsSize();
-		FHandleCharacterDeath->GetChildProperties() = Signature->GetChildProperties();
-		FHandleCharacterDeath->GetPropertiesSize() = Signature->GetPropertiesSize();
-		FHandleCharacterDeath->GetFunctionFlags() = (Signature->GetFunctionFlags() | FUNC_Native | FUNC_Public) & ~(FUNC_Delegate | FUNC_MulticastDelegate);
-
-		FHandleCharacterDeath->SetFuncPtr(&HandleCharacterDeath);
-		ObjectClass->GetFuncMap().Add(FHandleCharacterDeath->GetFName(), TObjectPtr<UFunction>(FHandleCharacterDeath));
+		// Создаем наш объект
+		FStaticConstructObjectParameters UObjectParams{UCustom, FoundPackage};
+		UObjectParams.Name = FName(STR("ModDelegateProxy"), FNAME_Add);
+		CallBackSucker = UObjectGlobals::StaticConstructObject(UObjectParams);
 	}
 
 	auto Destroy() -> void {
 		DataBaseConnector::Destroy();
+		// Самое важное что бы игра не пыталась обращаться к модификации, т.к. будут ошибки с памятью и хард краш
 		BindingManager.Destroy();
 		UObjectGlobals::UnregisterHook(FOnPlayerRespawned, FOnPlayerRespawnedIDs);
-		UObjectGlobals::UnregisterHook(FAttemptDestroy, FAttemptDestroyIDs);
-		UObjectGlobals::UnregisterHook(FAutosave, FAutosaveIDs);
 	}
 }
